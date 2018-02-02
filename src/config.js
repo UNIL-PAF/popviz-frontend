11e11e2ee68fa932bb959c4b93dc3b0ccf3a3005
rmylonas <roman.mylonas@isb-sib.ch>
2018-02-02 16:17:58 +0100

    version 1.0.8

--- src/config.js
+++ src/config.js
@@ -5,9 +5,9 @@
 
 var config = {};
 
-config.version = '1.0.7'
+config.version = '1.0.8'
 
-//config.urlBackend = "https://popviz.vital-it.ch/slimp/public"
-config.urlBackend = "http://localhost:3001"
+config.urlBackend = "https://popviz.vital-it.ch/slimp/public"
+//config.urlBackend = "http://localhost:3001"
 
 module.exports = config;
